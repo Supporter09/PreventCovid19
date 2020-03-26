4948f5787e79ab71db1d14a85b6e6bd1c73987cd
Supporter09(Minh) <51523860+Supporter09@users.noreply.github.com>
2020-03-26 09:05:39 +0700

    Anh Long sua phan Feedback ho em nhe

--- activar/js/quiz2.js
+++ activar/js/quiz2.js
@@ -107,84 +107,28 @@
     const submitButton = document.getElementById('submit');
     const myQuestions = [
       {
-        question: "Câu 1: Rửa tay trong bao lâu là đủ?",
+        question: "Who invented JavaScript?",
         answers: {
-          a: "Tối thiểu 20 giây",
-          b: "Tối đa 20 giây",
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "Câu 2: Rửa tay như thế nào để đảm bảo an toàn cho chính mình trong mùa dịch?",
-        answers: {
-          a: "Rửa tay trong trung bình 10 giây",
-          b: "Rửa tay thường xuyên với xà phòng và nước từ vòi",
+          a: "Douglas Crockford",
+          b: "Sheryl Sandberg",
         },
         correctAnswer: "b"
       },
       {
-        question: "Câu 3: Vị trí nào dễ bị bỏ qua khi rửa tay?",
+        question: "Which one of these is a JavaScript package manager?",
         answers: {
-          a: "Giữa móng và ngón tay",
-          b: "Kẽ ngón tay",
+          a: "Node.js",
+          b: "TypeScript",
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+          a: "Angular",
+          b: "jQuery",
         },
         correctAnswer: "b"
-      },
-      {
-        question: "Câu 4: Nên ưu tiên rửa tay bằng xà phòng với nước hay sử dụng nước rửa tay khô?",
-        answers: {
-          a: "Rửa tay dưới vòi nước với xà phòng là cách đơn giản và hiệu quả nhất",
-          b: "Nước rửa tay khô/cồn khô tốt hơn",
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "Câu 5: Sau mỗi lần cầm nắm vật dụng thường được động chạm, có cần phải rửa tay ngay không?",
-        answers: {
-          a: "Có",
-          b: "Không",
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "Câu 6: Khi rửa tay có nhất thiết phải dùng các sản phẩm tiệt trùng không hay có thể rửa bằng xà phòng thường?",
-        answers: {
-          a: "Không nhất thiết dùng sản phẩm tiệt trùng",
-          b: "Có nhất thiết dùng sản phẩm tiệt trùng",
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "Câu 7: Khi ho đã che miệng nhưng không có điều kiện rửa tay ngay thì nên làm như thế nào khi tiếp xúc với người khác?",
-        answers: {
-          a: "Lau tay bằng giấy ăn",
-          b: "Nhất thiết không tiếp xúc, chạm tay vào ai cho đến khi có điều kiện rửa tay",
-        },
-        correctAnswer: "b"
-      },
-      {
-        question: "Câu 8: Nên rửa tay bằng nước rửa tay sát khuẩn, nhưng trong đó lại có cồn, có an toàn với trẻ em không?",
-        answers: {
-          a: "Trẻ em sử dụng được nước rửa tay có cồn nhưng chỉ là loại 70%",
-          b: "Được, nước sát khuẩn có cồn không những an toàn cho trẻ nhỏ mà còn giúp diệt sạch vi khuẩn",
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "Câu 9: Rửa tay như nào sẽ đảm bảo đã rửa tay sạch?",
-        answers: {
-          a: "Miễn rửa tay với nước và xà phòng là được, bất kể trong bao lâu",
-          b: "Theo quy trình rửa tay thường quy 6 bước khuyến cáo bởi Bộ Y Tế để không bỏ sót bất cứ vị trí nào trên tay",
-        },
-        correctAnswer: "b"
-      },
-      {
-        question: "Câu 10: Ca khúc nào sau đây là ca khúc để tuyên truyền về covid19?",
-        answers: {
-          a: "Ghen Cô Vy",
-          b: "Ghen Cô Rô Na",
-        },
-        correctAnswer: "a"
       }
     ];
   
